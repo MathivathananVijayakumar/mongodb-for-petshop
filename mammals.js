f8a76efc27afad3dc476c30989fd2d1fcc283260
Mathivathanan <43021541+MathivathananVijayakumar@users.noreply.github.com>
2021-09-10 11:20:01 +0530

    Add files via upload

---
+++ mammals.js
@@ -0,0 +1,13 @@
+use petshop
+db.createCollection('mammals')
+db.mammals.insert({name:'dabour',species:'canis familiaris'})
+db.mammals.insert({name:'Alzation',species:'canis familiaris'})
+db.mammals.insert({name:'Rot weiler',species:'canis familiaris'})
+db.mammals.insert({name:'Manos Catery',species:'Felis Catus'})
+db.mammals.insert({name:'Balinese',species:'Felis Catus'})
+db.mammals.insert({name:'Bengal',species:'Felis Catus'})
+db.mammals.insert({name:'Kiskky',species:'Agpornis'})
+db.mammals.insert({name:'Moko',species:'Agpornis'})
+db.mammals.insert({name:'Bubbles',species:'Agpornis'})
+db.mamma
+ls.find()
